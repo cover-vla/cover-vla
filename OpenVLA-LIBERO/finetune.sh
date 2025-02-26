3be7b570384c69045b2c332222ef5aa961b7ce82
willxxy <willhan327@outlook.com>
2025-02-26 19:06:11 +0000

    del x ovla libero

--- OpenVLA-LIBERO/finetune.sh
+++
@@ -1,3 +0,0 @@
-python octo/examples/02_finetune_new_observation_action.py \
-    --pretrained_path=hf://rail-berkeley/octo-base \
-    --data_dir=/home/xilun/openvla/modified_libero_rlds
--- openpi/train.sh
+++ openpi/train.sh
@@ -1,4 +1,4 @@
 # CUDA_VISIBLE_DEVICES=2,3 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi0_fast_libero_low_mem_finetune --exp-name=libero_ft_low_mem_negative --overwrite
 
 
-CUDA_VISIBLE_DEVICES=2,3 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi0_fast_libero_low_mem_finetune --exp-name=libero_ft_low_mem_clip --overwrite
+CUDA_VISIBLE_DEVICES=2,3 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi0_fast_libero_low_mem_finetune --exp-name=libero_ft_low_mem_clip --overwritehttps://github.com/willxxy/vla-comp.git
