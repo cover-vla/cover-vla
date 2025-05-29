8900c3ae7aca0749db7e60c9a8769725475fefbc
XilunZhangRobo <xilunz@andrew.cmu.edu>
2025-05-29 03:29:36 +0000

    add openvla-oft

---
+++ openvla-oft/run_vla_oft.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=6 python experiments/robot/libero/run_libero_eval.py \
+  --pretrained_checkpoint moojink/openvla-7b-oft-finetuned-libero-spatial-object-goal-10 \
+  --task_suite_name libero_spatial
